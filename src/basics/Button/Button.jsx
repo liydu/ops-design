e151bbda70cf3802683967946cf9cdea718eb70f
Liki Du <Liki Du>
2016-07-06 18:09:52 +0800

    Add first unit test and got passed, yeah!

--- src/basics/Button/Button.jsx
+++ src/basics/Button/Button.jsx
@@ -1,11 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Button extends Component {
+  static propTypes = {
+    children: PropTypes.node,
+    href: PropTypes.string,
+    disabled: PropTypes.bool,
+    type: PropTypes.string,
+    onClick: PropTypes.func,
+  };
+
+  handleClick(ev) {
+    if (!this.props.disabled) this.props.onClick(ev);
+  }
+
   render() {
+    const { children, href, disabled, type, ...other } = this.props;
+
+    const hasHref = !!href;
+    // If it's a disabled link button, return a <span> instead
+    // And without other props
+    if (hasHref && disabled) {
+      return (
+        <span {...other}>{children}</span>
+      );
+    }
+
+    // The element is an <a> if it contains href attribute
+    const Element = hasHref ? 'a' : 'button';
+    // Assign other html attributes onto it
+    const buttonProps = {
+      ...other,
+      disabled,
+      href,
+      type,
+      onClick: ::this.handleClick,
+    };
+
     return (
-      <button>Button</button>
+      <Element {...buttonProps}>{children}</Element>
     );
   }
 }
 
 export default Button;
+
--- src/basics/Button/Button.spec.js
+++ src/basics/Button/Button.spec.js
@@ -24,11 +24,66 @@ describe('Spec of <Button />', () => {
     assert.ok(wrapper.text(), 'Button', 'should say button.');
     assert.ok(wrapper.is('a'), 'should match a <a> element.');
   });
+
   it('should render any container element');
-  it('should render children');
-  it('should render a disabled button when disabled={true}');
-  it('should render a dummy link button when href is provided and disabled={true}');
-  it('could be styelabe');
-  it('could overrides a default style');
-  it('could pass through other html attributes');
+
+  it('should render children', () => {
+    const children = <div>Button Children</div>;
+    const wrapper = shallowWithContext(
+      <Button>{children}</Button>
+    );
+    assert.ok(wrapper.contains(children), 'should contain children');
+  });
+
+  it('should render a disabled button when disabled={true}', () => {
+    const wrapper = shallowWithContext(
+      <Button disabled>Button</Button>
+    );
+    assert.ok(wrapper.text(), 'Button', 'should say button');
+    assert.ok(wrapper.is('button[disabled]'), 'should be a disabled button element');
+
+    // Should not be clickable
+    let clicked = false;
+    wrapper.setProps({
+      onClick: () => {
+        clicked = true;
+        return clicked;
+      },
+    });
+    wrapper.simulate('click');
+    assert.strictEqual(clicked, false, 'should not trigger the click');
+  });
+
+  it('should render a dummy link button when href is provided and disabled={true}', () => {
+    const wrapper = shallowWithContext(
+      <Button href="http://bing.com" disabled>Button</Button>
+    );
+    assert.ok(wrapper.text(), 'Button', 'should say button');
+    assert.notOk(wrapper.is('a'), 'should not match an <a> element');
+    assert.notOk(wrapper.is('button'), 'should not be a button element');
+    // A dummy link is just plain text
+    assert.notOk(wrapper.is('[href]'), 'should not have href attribute');
+  });
+
+  it('should be styelabe');
+  it('should overrides a default style');
+
+  it('should set the button type', () => {
+    const wrapper = shallowWithContext(
+      <Button type="submit">Button</Button>
+    );
+    assert.ok(wrapper.text(), 'Button', 'should say button');
+    assert.ok(wrapper.is('button[type="submit"]'), 'should have the type attribute');
+    // Set button type as reset
+    wrapper.setProps({ type: 'reset' });
+    assert.ok(wrapper.is('button[type="reset"]'), 'should have the type attribute');
+  });
+
+  it('should pass through other html attributes', () => {
+    const wrapper = shallowWithContext(
+      <Button name="hello">Button</Button>
+    );
+
+    assert.ok(wrapper.is('button[name="hello"]'), 'should have the name attribute');
+  });
 });
